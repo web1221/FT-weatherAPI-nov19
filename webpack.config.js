c89672baeae5871f7db08b0e70c9a3110d40cd24
Student <example@example.com>
2019-11-19 11:34:18 -0800

    add funtionality to weather project homework. need to do npm run start to access .env api_key

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const{ CleanWebpackPlugin} = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+
+module.exports = {
+  entry: './src/weather-interface.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins:[
+    new Dotenv(),
+    new UglifyJsPlugin({ sourceMap: true}),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'journal',
+      template: './src/weather.html',
+      inject: 'body'
+    }),
+],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
